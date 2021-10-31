8101063cbf040480c8c0d0e677d2eb1ea0d06d88
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-10-31 23:15:54 +0100

    constructor login

--- loginAdmin.js
+++ loginAdmin.js
@@ -1,6 +1,11 @@
 const farmacie = []
 let btnRegistrationForm = document.getElementById("btnRegistration")
 let nameNewMember = document.getElementById("nameNewMember")
+let cityNewMember = document.getElementById("cityNewMember")
+let addressNewMember = document.getElementById("via")
+let capNewMember = document.getElementById("cap")
+let telefonoNewMember = document.getElementById("telefono")
+let emailNewMember = document.getElementById("email")
 let idMember = farmacie.length;
 
 // CONSTRUCTOR
@@ -8,9 +13,15 @@ let idMember = farmacie.length;
 function Member(name, city) {
     this.name = name;
     this.city = city;
+    this.via = via;
+    this.cap = cap;
+    this.telefono = telefono;
+    this.email = email;
 };
 
 btnRegistrationForm.addEventListener("click", function() {
-    farmacie[farmacie.length] = new Member(`${nameNewMember.value}`, "Como");
+
+    farmacie[farmacie.length] = new Member(`${nameNewMember.value}`, `${cityNewMember.value}`, `${addressNewMember.value}`,
+        `${capNewMember.value}`, `${telefonoNewMember.value}`, `${idMember.value}`);
+    console.log(farmacie)
 })
-console.log(farmacie)
